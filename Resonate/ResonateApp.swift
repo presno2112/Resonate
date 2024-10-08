9c605231e325ae26eb74eddc5ffd2009f0336b03
Sebastian Presno <presno@10-20-51-227.0200.fed.it.iosda.org>
2024-10-08 14:59:00 +0200

    Initial commit

---
+++ Resonate/ResonateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResonateApp.swift
+//  Resonate
+//
+//  Created by Sebastian Presno on 08/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ResonateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
